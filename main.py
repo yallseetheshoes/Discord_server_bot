46106e58f0387000c1b16c304300bca7202d7401
MaggiMoggi <private>
2025-11-24 05:18:49 +0100

    Add miscellaneous improvements

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import discord
 from discord.ext import commands
+import json
 import socket
+import os
 
-bot = commands.Bot()
+bot = commands.Bot(command_prefix='!',intents=discord.Intents.none())
 
+# load channel id from json file
+with json.open("data.json") as f: 
+    channel_id = json.load(f)["channel_id"]
+
+
+
+
+# get bot token from env variable
+def get_token():
+        token = os.getenv("Token")
+        if token is None:
+            print("Error reading env did you forget to add it in system variables?")
+            exit()
+        return token
+
+
+# check if the minecraft server is online
 def is_online(host="127.0.0.1", port=25565):
     try:
         with socket.create_connection((host, port), timeout=2):
@@ -11,4 +30,10 @@ def is_online(host="127.0.0.1", port=25565):
     except:
         return False
 
-bot.run("")
+# print when bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+
+# run
+bot.run(get_token())
