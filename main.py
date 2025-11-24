fdcfd69eed89fa40009dc7e858a681be3092bf2c
MaggiMoggi <private>
2025-11-24 17:48:02 +0100

    Implement server status check

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 import socket
 import os
 
+
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.none())
 
 # load channel id from json file
-with json.open("data.json") as f: 
+with open("data.json") as f: 
     channel_id = json.load(f)["channel_id"]
 
 
@@ -30,10 +31,20 @@ def is_online(host="127.0.0.1", port=25565):
     except:
         return False
 
+
 # print when bot is ready
 @bot.event
 async def on_ready():
+    infochannel = await bot.fetch_channel(channel_id)
+    check_server_status.start()
+    #await infochannel.send(f"Server status: {is_online()}")
     print(f'Logged in as {bot.user}!')
 
+@tasks.loop(seconds=10)
+async def check_server_status():
+    online = is_online()
+    status = discord.Status.online if online else discord.Status.dnd
+    activity = discord.Game(("Online" if online else "Offline"))
+    await bot.change_presence(status=status,activity=activity)
 # run
 bot.run(get_token())
